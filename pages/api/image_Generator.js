89801c5f8f5a5bc585ba8c560cb6abb0f52b195c
alu0101330037 <alu0101330037@ull.edu.es>
2023-01-13 13:31:20 +0000

    Actualización

---
+++ pages/api/image_Generator.js
@@ -0,0 +1,17 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  console.log(req.body);
+  const response = await openai.createImage({
+      prompt: req.body.prompt,
+      n: 1,
+      size: req.body.size,
+    });
+    const image_url = response.data.data[0].url;
+    res.status(200).json({ result: image_url });
+}
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,16 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import Link from 'next/link'
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
-
-  async function onSubmit(event) {
-    event.preventDefault();
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ animal: animalInput }),
-    });
-    const data = await response.json();
-    setResult(data.result);
-    setAnimalInput("");
-  }
   return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+    <ul>
+      <h1>
+        
+        <center><Link href='/pet'>  Generador de nombres de mascotas </Link></center>
+        
+        <center><Link href='/image'> Generador imagenes </Link></center>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
-  );
+      
+      </h1>
+    </ul>
+  )
 }
